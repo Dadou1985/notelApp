7284e50be89e179eea6a8bfa2a97904fba23ac1c
dadou1985 <davidsimba1985@yahoo.com>
2020-11-10 23:58:15 +0100

    (messageCommunIzi.js) implement mod status: in progress

---
+++ .history/src/components/section/messageCommunizi_20201110214420.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState, useContext } from 'react'
+import {Button} from 'reactstrap'
+import Avatar from 'react-avatar'
+import { FirebaseContext } from '../../Firebase'
+
+
+  const MessageCommunizi = ({author, text, hour, date, markup}) =>{
+    
+    const [info, setInfo] = useState([])
+    const dateNote = date.toString()
+    const { user, firebase } = useContext(FirebaseContext)
+
+    Date.prototype.yyyymmdd = function() {
+      let day = this.getDate()
+      let month = this.getMonth()
+      let calendar = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+      let year = this.getFullYear()
+
+      let date = day + " " + calendar[month] + " " + year
+      return date
+  };
+
+  let dayIn = new Date()
+  let today = dayIn.yyyymmdd()
+
+  useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
+    firebase.moodOnAir({signal : signal}).onSnapshot(function(snapshot) {
+                const snapInfo = []
+              snapshot.forEach(function(doc) {          
+                snapInfo.push({
+                    id: doc.id,
+                    ...doc.data()
+                  })        
+                });
+                console.log(snapInfo)
+                setInfo(snapInfo)
+            });
+            return () => {
+                abortController.abort()
+            }
+ },[])
+console.log(info)
+
+    
+            return (
+            <div>
+              <h6>{author}</h6>
+              <p>{text}</p>
+              <i>écrit le {date} à {hour} </i>
+            </div>)
+            
+    
+  }
+
+  export default MessageCommunizi
