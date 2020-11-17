8acec189a5b9d9dd6db54080d5fcbbdbabdfd613
dadou1985 <davidsimba1985@yahoo.com>
2020-11-17 12:37:53 +0100

    (deepMap.js) create details window: in progress

--- src/components/section/deepMap2.js
+++ src/components/section/deepMap2.js
@@ -3,11 +3,20 @@ import Map, {Marker} from 'react-map-gl'
 import OverbookingBox from './overbookingBox'
 import RedBar from './redBar'
 import {Form, Button, Modal, OverlayTrigger, Tooltip} from 'react-bootstrap'
-import MarkerImg from './markerImg'
+import MarkerImg from './markerImgAdvisor'
+import * as hotelData from '../../../hotel.json'
+import Like from '../../svg/stars.svg'
+import Team from '../../svg/team.svg'
+import Management from '../../svg/management.svg'
+import Customer from '../../svg/customer.svg'
+import Wage from '../../svg/wage.svg'
+import Divider from '@material-ui/core/Divider'
+import Arrow from '../../svg/arrowDown.svg'
+import ToggleDisplay from 'react-toggle-display'
+import Close from '../../svg/close.svg'
 
 
-
-export default function DeepMap({user, firebase}) {
+export default function DeepMap2({user, firebase}) {
 
     const [info, setInfo] = useState([])
     const [selectedHotel, setselectedHotel] = useState(null)
@@ -16,6 +25,10 @@ export default function DeepMap({user, firebase}) {
     const [filter, setFilter] = useState("Ile-de-France")
     const [geo, setGeo] = useState({geo: []})
     const [operator, setOperator] = useState("==")
+    const [openData, setOpenData] = useState([])
+    const [show, setShow] = useState(false)
+    const [details, setDetails] = useState(false)
+    const [comment, setComment] = useState(false)
 
     const yes = "oui"
     const no = "non"
@@ -30,6 +43,35 @@ export default function DeepMap({user, firebase}) {
           [event.target.name]: event.target.value
         }))
       }
+
+      const handleShowDetails = () =>{
+          let arrowTop = document.getElementById("arrowTop")
+        if(arrowTop.style.transform === "rotate(0turn)"){
+            setShow(!show)
+            return arrowTop.style.transform = "rotate(0.5turn)"
+        }
+        if(arrowTop.style.transform === "rotate(0.5turn)"){
+            setShow(!show)
+            return arrowTop.style.transform = "rotate(0turn)"
+        }
+        
+    }  
+
+    const handleShowComment = () =>{
+        let arrowBottom = document.getElementById("arrowBottom")
+        if(arrowBottom.style.transform === "rotate(0turn)"){
+            setComment(!comment)
+            return arrowBottom.style.transform = "rotate(0.5turn)"
+        }
+        if(arrowBottom.style.transform === "rotate(0.5turn)"){
+            setComment(!comment)
+            return arrowBottom.style.transform = "rotate(0turn)"
+        }
+    }  
+
+    const handleShowWindowDetails = () =>{
+        setDetails(false)
+    }
 
     const handleClose = () => setList(false)
     const handleShow = () => setList(true)
@@ -82,6 +124,11 @@ export default function DeepMap({user, firebase}) {
                     abortController.abort()
                     }
      },[field, filter, operator])
+
+     
+
+     console.log(hotelData)
+
      
     return (
         <div
@@ -98,35 +145,182 @@ export default function DeepMap({user, firebase}) {
                 setviewPort(viewPort)
             }}
             >
-                {info.map(hotel =>(
+                {hotelData.map((hotel, key) =>(
                     <Marker 
-                    key={hotel.id}
-                    latitude={hotel.lat} 
-                    longitude={hotel.lng}
+                    key={key}
+                    latitude={hotel.geometry.coordinates[1]} 
+                    longitude={hotel.geometry.coordinates[0]}
                      > 
                      <OverlayTrigger
                         placement="top"
                         overlay={
                         <Tooltip id="title">
-                            <h5>{hotel.hotelName}</h5>
-                            <b>Rack : {hotel.rac}€</b>
-                            <h6 className="text-success">{hotel.roomAvailable} chambre(s) restante(s)</h6>
+                            <h5 style={{padding: "5%"}}>{hotel.properties.nom_commercial}</h5>
+                            <span style={{color: "green"}}>
+                               <b>4</b> <img src={Like} alt="like" style={{width: "10%", background: "green", marginLeft: "1px"}} />
+                            </span>
                         </Tooltip>
                         }>
                             <button style={{background: "none", border: "none"}}
                             onClick={(event) => {
                                     event.preventDefault()
                                     setselectedHotel(hotel)
-                                    handleShow()
+                                    setDetails(true)
                                 }}>
-                                <MarkerImg room={hotel.roomAvailable} />
+                                <MarkerImg />
                             </button>
                      </OverlayTrigger>
                     </Marker>
                 ))}
                 
                 {selectedHotel ? (
-                    
+                    <>
+                        <ToggleDisplay show={details}>
+                            <div style={{
+                                display: "flex",
+                                flexFlow: "column",
+                                justifyContent: "flex-start",
+                                position: "relative",
+                                width: "100%",
+                                height: "100%",
+                                maxHeight: "auto",
+                                backgroundColor: "rgb(67, 66, 66)",
+                                zIndex: "500",
+                                padding: "3%",
+                                opacity: "0.9",
+                                overflow: "auto"
+                            }}
+                            defaultChecked={details}>
+                                <div style={{
+                                    display: "flex",
+                                    flexFlow: "row",
+                                    justifyContent: "space-between",
+                                    alignItems: "flex-start",
+                                    marginBottom: "4vh"
+                                }}>
+                                    <div style={{diplay: "flex", flexFlow: "row"}}>
+                                        <h3>{selectedHotel.properties.nom_commercial}</h3>
+                                        <span>Etablissement {selectedHotel.properties.classement}</span>
+                                        <img src={Arrow} alt="arrow" style={{width: "1vw", cursor: "pointer", filter: "invert(100%)", marginLeft: "1vw", transform: "rotate(0turn)"}} id="arrowTop" onClick={handleShowDetails} />
+                                    </div>
+                                    <img src={Close}  alt="" style={{width:"2vw", filter: "invert(100%)", cursor: "pointer"}} onClick={handleShowWindowDetails} />
+                                </div>
+                            <ToggleDisplay show={show}>
+                                    <div style={{
+                                        display: "flex",
+                                        flexFlow: "column", 
+                                        width: "90%",
+                                        marginBottom: "4vh"
+                                    }}
+                                    defaultChecked={show}>
+                                        <Divider style={{height: "1vh", marginBottom: "4vh"}} />
+                                        <span><b style={{color: "gray"}}>Adresse: </b> {selectedHotel.properties.adresse}, {selectedHotel.properties.code_postal} {selectedHotel.properties.commune}</span>
+                                        <span><b style={{color: "gray"}}>Nombre de chambres: </b> {selectedHotel.properties.nombre_de_chambres}</span> 
+                                        <span><b style={{color: "gray"}}>Type d'établissement: </b>{selectedHotel.properties.typologie_etablissement}</span>
+                                        <span><b style={{color: "gray"}}>Téléphone: </b>0{selectedHotel.properties.telephone}</span> 
+                                        <span><b style={{color: "gray"}}>Courriel: </b>{selectedHotel.properties.courriel}</span>
+                                        <span><b style={{color: "gray"}}>Portail web: </b>{selectedHotel.properties.site_internet}</span>
+                                    </div>
+                            </ToggleDisplay>
+                                <Divider style={{height: "1vh", marginBottom: "4vh"}} />
+                                <div style={{
+                                    display: "flex",
+                                    flexFlow: "column",
+                                    alignItems: "center",
+                                    marginBottom: "1vh"
+                                }}>
+                                    <h3>Statistiques de l'établissement</h3>
+                                    <div style={{
+                                    display: "flex",
+                                    flexFlow: "row",
+                                    justifyContent: "space-around",
+                                    marginTop: "4vh"
+                                }}>
+                                        <div style={{
+                                            display: "flex",
+                                            flexFlow: "column",
+                                            alignItems: "center",
+                                            width: "25%"
+                                        }}>
+                                            <h5>Team</h5>
+                                            <img src={Team} alt="" style={{width: "30%", borderRadius: "25px", marginBottom: "2vh"}} />
+                                            <p style={{color: "green", fontSize: "2em"}}>4.5</p>
+                                        </div>
+                                        <div style={{
+                                            display: "flex",
+                                            flexFlow: "column",
+                                            alignItems: "center",
+                                            width: "25%"
+                                        }}>
+                                            <h5>Management</h5>
+                                            <img src={Management} alt="" style={{width: "30%", borderRadius: "25px", marginBottom: "2vh"}} />
+                                            <p style={{color: "green", fontSize: "2em"}}>4.5</p>
+                                        </div>
+                                        <div style={{
+                                            display: "flex",
+                                            flexFlow: "column",
+                                            alignItems: "center",
+                                            width: "25%"
+                                        }}>
+                                            <h5>Clientèle</h5>
+                                            <img src={Customer} alt="" style={{width: "30%", borderRadius: "25px", marginBottom: "2vh"}} />
+                                            <p style={{color: "green", fontSize: "2em"}}>4.5</p>
+                                        </div>
+                                        <div style={{
+                                            display: "flex",
+                                            flexFlow: "column",
+                                            alignItems: "center",
+                                            width: "25%"
+                                        }}>
+                                            <h5>Salaire</h5>
+                                            <img src={Wage} alt="" style={{width: "30%", borderRadius: "25px", marginBottom: "2vh"}} />
+                                            <p style={{color: "green", fontSize: "2em"}}>4.5</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div style={{display: "flex", flexFlow: "column", justifyContent: "center", marginBottom: "5vh"}}>
+                                    <Divider style={{height: "1vh", marginBottom: "5vh"}} />
+                                    <div style={{
+                                        display: "flex",
+                                        flexFlow: "row",
+                                        justifyContent: "space-between",
+                                        alignItems: "center"}}>
+                                        <Button size="lg" style={{width: "88%"}} onClick={handleShow}>Ajouter un avis</Button>
+                                        <span>
+                                           9 avis 
+                                            <img src={Arrow} alt="arrow" style={{width: "1vw", cursor: "pointer", filter: "invert(100%)", marginLeft: "1vw", transform: "rotate(0turn)"}} id="arrowBottom" onClick={handleShowComment} />
+                                        </span>
+                                    </div>
+                                </div>
+                                <ToggleDisplay show={comment}>
+                                    <div style={{
+                                            display: "flex",
+                                            flexFlow: "column",
+                                            padding: "2%", 
+                                            backgroundColor: "rgb(33, 35, 39)",
+                                            borderRadius: "15px"
+                                        }}
+                                        defaultChecked={comment}>
+                                        <div>
+                                            <h4>"I'm in love with this hotel"</h4>
+                                            <p>Ancien employé</p>
+                                        </div>
+                                        <div>
+                                            <h6>Le Best Of</h6>
+                                            <p>DATAtourisme est un dispositif national de collecte et de diffusion en open data des données touristiques institutionnelles. Porté par l’Etat, il est né d’une coopération inédite avec les réseaux des offices de tourisme, des agences départementales et des comités régionaux du tourisme de l’ensemble du territoire national.</p>
+                                        </div>
+                                        <div>
+                                            <h6>Les BullShift</h6>
+                                            <p>DATAtourisme est un dispositif national de collecte et de diffusion en open data des données touristiques institutionnelles. Porté par l’Etat, il est né d’une coopération inédite avec les réseaux des offices de tourisme, des agences départementales et des comités régionaux du tourisme de l’ensemble du territoire national.</p>
+                                        </div>
+                                        <div style={{textAlign: "right"}}>Mardi 17 novembre 2020</div>
+                                    </div>
+                                </ToggleDisplay>
+                            </div>
+                        </ToggleDisplay>
+                            
+                            
                             <Modal show={list}
                                 size="lg"
                                 aria-labelledby="contained-modal-title-vcenter"
@@ -213,39 +407,12 @@ export default function DeepMap({user, firebase}) {
                                 onClick={(event) => {
                                     event.preventDefault()
                                     setformValue("")
-                                    let marker = Date.now()
-                                    let token = Date.now()
-                                    firebase.addOverbookIn({
-                                        hotelName: formValue.hotelName, 
-                                        totalRoom: formValue.totalRoom, 
-                                        totalNight: formValue.totalNight, 
-                                        client: formValue.client, 
-                                        markup: marker, 
-                                        token: token,
-                                        initialPrice: formValue.initialPrice, 
-                                        pec: formValue.pec, 
-                                        pax: formValue.pax, 
-                                        refHotel: formValue.refHotel, 
-                                        doc: selectedHotel.id, 
-                                        status: "en attente"})
-                                    firebase.addOverbookOut({
-                                        hotelName: selectedHotel.hotelName, 
-                                        totalRoom: formValue.totalRoom, 
-                                        totalNight: formValue.totalNight, 
-                                        client: formValue.client, 
-                                        markup: marker, 
-                                        token: token,
-                                        initialPrice: formValue.initialPrice, 
-                                        pec: formValue.pec, 
-                                        pax: formValue.pax, 
-                                        refHotel: formValue.refHotel, 
-                                        doc: user.displayName, 
-                                        status: "en attente"})
+                                    
                                     setselectedHotel(null)
                                 }}>Déloger</Button>
                                 </Modal.Footer>
                             </Modal>                    
-                ) : null}
+                </>) : null}
             </Map>
             <div style={{
                     display: "flex",
@@ -256,7 +423,7 @@ export default function DeepMap({user, firebase}) {
                     marginTop: "2%",
                     marginBottom: "2%"
                   }}>
-                <h5 className="text-center" style={{marginBottom: "5%"}}><b>Overbooking</b></h5>
+                <h5 className="text-center" style={{marginBottom: "5%"}}><b>Shift Advisor</b></h5>
                 
                 <h6 className="text-center"><b>Filtrer les recherches d'hôtels par :</b></h6>
                 <div style={{
@@ -303,8 +470,6 @@ export default function DeepMap({user, firebase}) {
                     </Form.Group>
                 </Form.Row>
                 </div>
-                <OverbookingBox />
-                <RedBar />
             </div>
             
         </div>
---
+++ src/components/section/markerImgAdvisor.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hotel from '../../svg/hostel.png'
+
+export default function MarkerImgAdvisor() {
+
+    return (
+        <div>
+            <img src={Hotel} alt="hotel" style={{width: "40px"}} />
+        </div>
+    )
+
+}
