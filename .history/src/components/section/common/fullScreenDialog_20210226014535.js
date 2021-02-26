478866d3d2e5f7c58c04cb4416567c7c706d21e6
dadou1985 <davidsimba1985@yahoo.com>
2021-02-26 03:03:19 +0100

    (dilema.js) responsive design : done

---
+++ .history/src/components/section/common/fullScreenDialog_20210226014535.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import Slide from '@material-ui/core/Slide';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function FullScreenDialog({children, showDialog, hideShow}) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Dialog fullScreen open={showDialog} onClose={hideShow} TransitionComponent={Transition}>
+        <main style={{width: "100vw", height: "100vh"}}>{children}</main>
+        <div style={{
+            position: "fixed",
+            bottom: "0px",
+            display: "flex",
+            flexFlow: "row",
+            justifyContent: "center"
+            alignItems: "center",
+            width: "100vw",
+            height: "10vh",
+            backgroundColor: "lightgrey"
+        }}>Revenir au porail</div>
+      </Dialog>
+    </div>
+  );
+}
---
+++ .history/src/components/section/common/fullScreenDialog_20210226023426.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import Slide from '@material-ui/core/Slide';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+}));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function FullScreenDialog({children, showDialog, hideShow}) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Dialog fullScreen open={showDialog} onClose={hideShow} TransitionComponent={Transition}>
+        <div style={{width: "100vw", height: "100"}}>
+            <main style={{width: "90%", height: "90%"}}>{children}</main>
+            <div style={{
+                position: "fixed",
+                bottom: "0px",
+                display: "flex",
+                flexFlow: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100vw",
+                height: "10vh",
+                fontSize: "20px",
+                color: "gray",
+                backgroundColor: "ghostwhite"
+            }}
+            onClick={hideShow}>Revenir au portail</div>
+        </div>
+      </Dialog>
+    </div>
+  );
+}
